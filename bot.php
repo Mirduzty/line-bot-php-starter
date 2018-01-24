cf2aebf0826183ca2eb96ce2d7efe5f2a9adb197
Mirduzty <35687210+Mirduzty@users.noreply.github.com>
2018-01-24 10:02:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -42,7 +42,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-		}else{
+		}else if($event['type'] != ''){
 		
 			
 			// Get text sent
@@ -75,6 +75,54 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
+			echo $result . "\r\n";
+			
+			
+			$url = 'https://api.line.me/v2/bot/profile/'.$event['source']['userId'];
+			
+			$headers = array('Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+			
+			
+		}else if($event['displayName']){
+		
+			// Get text sent
+			
+			$text = $event['displayName'].'</br>'.$event['userId'];
+			// Get replyToken
+			$to = $event['userId'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'to' => $to,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$url = 'https://api.line.me/v2/bot/message/push';
+			
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);	
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 			echo $result . "\r\n";
 			
 			
