ff1c950eaf35c6dfb57a2b3d59e140b3d06e8284
Mirduzty <35687210+Mirduzty@users.noreply.github.com>
2018-01-24 17:01:44 +0700

    Create send_group.php

---
+++ send_group.php
@@ -0,0 +1,33 @@
+<?php
+date_default_timezone_set("Asia/Bangkok");
+$access_token = 'I/LJS0JniZFVPHHlag/+mp8HpLqhtizFs9Pxj6UkdJnVio4lIMmjnhB8/OFYN4CTwNoa1f1hnvV74wnc7eFFYn5Y70qi3eFCXSO5IfFKf82oyLS0dMVyeAAxUE/DI24J+5XM5p4Y4QCJf5on24aemgdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+
+  $text = 'ทดสอบ Bot ส่งข้อมความ';
+	// Get replyToken
+	$to = 'Cb71b716341c030faec503e47720f812a';
+	// Build message to reply back
+	$messages = [
+		'type' => 'text',
+		'text' => $text
+	];
+	$data = [
+		'to' => $to,
+		'messages' => [$messages],
+	];
+	$post = json_encode($data);
+	$url = 'https://api.line.me/v2/bot/message/push';
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);	
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+
+echo "OK";
